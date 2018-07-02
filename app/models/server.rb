dad81f96a2d8d36d6e356681a4cd6e498e46ad31
Louis <gofaceme@gmail.com>
2018-07-03 02:03:13 +0800

    model: test

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,6 +2,8 @@ class Server < ApplicationRecord
   belongs_to :server_size
   has_many :vms, dependent: :destroy
 
-  validates :ip, :server_size_id, presence: true
+  validates :ip, :server_size_id, :sn, :os, :purchase_time, presence: true
   validates :ip, uniqueness: true
+
+  default_scope -> { order(ip: :asc) }
 end
--- test/models/server_test.rb
+++ test/models/server_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ServerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "server attributes must not be empty" do
+    server = Server.new
+    assert server.invalid?
+    assert server.errors[:ip].any?
+    assert server.errors[:server_size_id].any?
+    assert server.errors[:sn].any?
+    assert server.errors[:os].any?
+    assert server.errors[:purchase_time].any?
+  end
 end
